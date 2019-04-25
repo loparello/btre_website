da3371987bc40403ac3b6348bccb406fa948d343
Lorenzo Parenti <lorenzo@flowmo.co>
2019-04-25 14:56:32 +0100

    Added dynamic listing and pagination

--- listings/views.py
+++ listings/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
 def index(request):
-  listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-  context = {
-    'listings': listings
-  }
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listing = paginator.get_page(page)
 
-  return render(request, 'listings/listings.html', context)
+    context = {
+        'listings': paged_listing
+    }
 
-def listing(request):
-  return render(request, 'listings/listing.html')
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html')
 
 def search(request):
-  return render(request, 'listings/search.html')
+    return render(request, 'listings/search.html')
